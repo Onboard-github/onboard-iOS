3404ac6467660c2029753b2cc88911fa00371160
윤다예 <daye.elin.yoon@gmail.com>
2023-09-17 22:21:56 +0900

    TestReactor 작성

--- onboard-iOS/Presentation/Test/TestReactor.swift
+++ onboard-iOS/Presentation/Test/TestReactor.swift
@@ -7,3 +7,54 @@
 
 import Foundation
 
+import ReactorKit
+
+final class TestReactor: Reactor {
+
+    var initialState: State = .init()
+
+    enum Action {
+        case testAPI
+        case kakao
+        case google
+        case apple
+    }
+
+    enum Mutation {
+        case setLoginResult(token: String)
+    }
+
+    struct State {
+        var result: String = ""
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .testAPI:
+            return .empty()
+
+        case .apple:
+            // TODO: Apple Login
+            return .empty()
+
+        case .google:
+            // TODO: Google Login
+            return .empty()
+
+        case .kakao:
+            // TODO: Kakao Login
+            return .empty()
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+
+        switch mutation {
+        case let .setLoginResult(token):
+            state.result = token
+        }
+
+        return state
+    }
+}
