17fd52546c179999defdab383bcd96bc4d0d830a
윤다예 <daye.elin.yoon@gmail.com>
2023-09-17 22:33:28 +0900

    testViewController 작성

--- onboard-iOS/Presentation/Test/TestViewController.swift
+++ onboard-iOS/Presentation/Test/TestViewController.swift
@@ -5,8 +5,37 @@
 //  Created by Daye on 2023/09/17.
 //
 
-import Foundation
+import UIKit
 
-final class TestViewController {
-    
+import ReactorKit
+
+final class TestViewController: UIViewController, View {
+
+    typealias Reactor = TestReactor
+
+    // MARK: - Properties
+
+    var disposeBag = DisposeBag()
+
+    // MARK: - Life Cycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Bind
+
+    func bind(reactor: TestReactor) {
+        self.bindAction(reactor: reactor)
+        self.bindState(reactor: reactor)
+    }
+
+    private func bindAction(reactor: TestReactor) {
+        
+    }
+
+    private func bindState(reactor: TestReactor) {
+
+    }
+
 }
