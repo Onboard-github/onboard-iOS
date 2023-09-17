b3c49b56d633b89dd094af9ecf0b5d18ff8d68a8
윤다예 <daye.elin.yoon@gmail.com>
2023-09-17 23:30:08 +0900

    AppleLoginManager 생성

---
+++ onboard-iOS/Repository/SocialLogin/AppleLoginManager.swift
@@ -0,0 +1,14 @@
+//
+//  AppleLoginManager.swift
+//  onboard-iOS
+//
+//  Created by Daye on 2023/09/17.
+//
+
+import Foundation
+
+import AuthenticationServices
+
+final class AppleLoginManager {
+
+}
