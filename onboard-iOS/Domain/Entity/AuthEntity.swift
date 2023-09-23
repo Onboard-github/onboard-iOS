9bbcae1b9a7d45c4233033668cffb9959c045f8e
윤다예 <daye.elin.yoon@gmail.com>
2023-09-23 18:00:02 +0900

    Entity 구조 수정

--- onboard-iOS/Domain/Entity/AuthEntity.swift
+++ onboard-iOS/Domain/Entity/AuthEntity.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
-struct AuthEntity {
-    let accessToken: String
-    let refreshToken: String
+enum AuthEntity {
+    struct Res {
+        let accessToken: String?
+        let refreshToken: String?
+    }
+
+    struct Req {
+        let type: AuthType
+        let token: String
+
+        enum AuthType: String {
+            case kakao = "KAKAO_ACCESS_TOKEN"
+            case google = "GOOGLE_ID_TOKEN"
+            case apple = "APPLE_ID_TOKEN"
+            case refresh = "REFRESH"
+        }
+    }
 }
