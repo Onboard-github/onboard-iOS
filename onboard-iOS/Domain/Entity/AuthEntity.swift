58a66251318d4dff56c8b1e3d8865dd33c0c5e34
hyeri00 <nexusring@gmail.com>
2023-10-02 02:04:17 +0900

    사용자 인증과 관련된 정보 모델링

--- onboard-iOS/Domain/Entity/AuthEntity.swift
+++ onboard-iOS/Domain/Entity/AuthEntity.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+enum AuthEntity {
+    struct Res {
+        let accessToken: String?
+        let refreshToken: String?
+    }
+
+    struct Req {
+        let type: AuthType
+        let token: String
+
+        enum AuthType: String {
+            case kakao = "KAKAO_ACCESS_TOKEN"
+            case google = "GOOGLE_ID_TOKEN"
+            case apple = "APPLE_ID_TOKEN"
+            case refresh = "REFRESH"
+        }
+    }
+}
