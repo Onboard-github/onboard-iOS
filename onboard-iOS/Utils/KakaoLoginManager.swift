ced42158df182462beb6e96062978172e2ea7d1d
higgs1022 <csh8130@kakao.com>
2023-10-03 17:44:08 +0900

    [ONB-10] 카카오 oauth Token 획득 후 onboard 서버에 JWT 토큰으로 전달

--- onboard-iOS/Utils/KakaoLoginManager.swift
+++ onboard-iOS/Utils/KakaoLoginManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import KakaoSDKUser
 
 protocol KakaoLoginManager {
     func excute(delegate: KakaoLoginDelegate)
@@ -23,6 +24,53 @@ final class KakaoLoginManagerImpl: NSObject, KakaoLoginManager {
 
     func excute(delegate: KakaoLoginDelegate) {
         self.delegate = delegate
-        print("kakao login button hit")
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            kakaoTalkLogin()
+        } else {
+            kakaoAccountLogin()
+        }
+    }
+}
+
+extension KakaoLoginManagerImpl {
+    private func kakaoTalkLogin() {
+        UserApi.shared.loginWithKakaoTalk {[weak self] (oauthToken, error) in
+            guard let self = self else { return }
+            guard let oauthToken = oauthToken else {
+                if let error = error {
+                    print(error)
+                }
+                return
+            }
+            
+            self.token = self.oAuthToJWT(accessToken: oauthToken.accessToken)
+        }
+    }
+    
+    private func kakaoAccountLogin() {
+        UserApi.shared.loginWithKakaoAccount {[weak self] (oauthToken, error) in
+            guard let self = self else { return }
+            guard let oauthToken = oauthToken else {
+                if let error = error {
+                    print(error)
+                }
+                return
+            }
+            
+            self.token = self.oAuthToJWT(accessToken: oauthToken.accessToken)
+        }
+    }
+    
+    private func oAuthToJWT(accessToken: String) -> String {
+        let header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}"
+        let payload = "{\"access_token\":\"\(accessToken)\"}"
+
+        // 헤더와 페이로드를 base64 인코딩합니다.
+        let headerData = header.data(using: .utf8)!.base64EncodedString()
+        let payloadData = payload.data(using: .utf8)!.base64EncodedString()
+
+        // 헤더와 페이로드를 JWT 형식으로 연결하여 JWT 문자열을 생성합니다.
+        let jwt = "\(headerData).\(payloadData)"
+        return jwt
     }
 }
