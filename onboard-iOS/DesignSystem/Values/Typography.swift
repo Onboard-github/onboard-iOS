0973bfce6081cd958324c1b3a01f262986d54283
hyeri00 <nexusring@gmail.com>
2023-10-06 00:54:56 +0900

    Font enum으로 구조화

---
+++ onboard-iOS/DesignSystem/Values/Typography.swift
@@ -0,0 +1,15 @@
+//
+//  Typography.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 2023/10/06.
+//
+
+import UIKit
+
+enum Font: String {
+    case bold = "SpoqaHanSansNeo-Bold"
+    case medium = "SpoqaHanSansNeo-Medium"
+    case regular = "SpoqaHanSansNeo-Regular"
+    case light = "SpoqaHanSansNeo-Light"
+}
