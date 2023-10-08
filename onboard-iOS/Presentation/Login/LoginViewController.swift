7000b549648ef021243bab79fc3f1f1e24e448b9
윤다예 <daye.elin.yoon@gmail.com>
2023-10-08 20:18:06 +0900

    #ONB-8 Login VC, Reactor 생성

---
+++ onboard-iOS/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,71 @@
+//
+//  LoginViewController.swift
+//  onboard-iOS
+//
+//  Created by Daye on 2023/10/08.
+//
+
+import UIKit
+
+import ReactorKit
+
+final class LoginViewController: UIViewController, View {
+
+    typealias Reactor = TestReactor
+
+    // MARK: - Properties
+
+    var disposeBag = DisposeBag()
+
+    private let testView = TestView()
+
+    // MARK: - Life Cycles
+
+    override func loadView() {
+        self.view = testView
+    }
+
+    init(reactor: TestReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Bind
+
+    func bind(reactor: TestReactor) {
+        self.bindAction(reactor: reactor)
+        self.bindState(reactor: reactor)
+    }
+
+    private func bindAction(reactor: TestReactor) {
+        self.testView.googleLoginButtonAction = {
+            reactor.action.onNext(.google)
+        }
+
+        self.testView.didTapAppleButton = {
+            reactor.action.onNext(.apple)
+        }
+
+        self.testView.didTapKakaoButton = {
+            reactor.action.onNext(.kakao)
+        }
+    }
+
+    private func bindState(reactor: TestReactor) {
+        reactor.state
+            .map { $0.result }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] result in
+                self?.testView.bind(text: result)
+            })
+            .disposed(by: self.disposeBag)
+    }
+
+    @objc private func buttonAction() {
+        GoogleLoginManager.shared.signIn(withPresenting: self)
+    }
+}
