23ad2324c276b3bd1f2b9de44ebaa55d45383354
윤다예 <daye.elin.yoon@gmail.com>
2023-10-09 15:45:21 +0900

    #onb-12 tableView 관련 extension 작성

---
+++ onboard-iOS/Extensions/UITableViewCell+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+Extension.swift
+//  onboard-iOS
+//
+//  Created by Daye on 2023/10/09.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
