92f8c65e80d4cda6e3d5a093ba8cde84df513e4a
윤다예 <daye.elin.yoon@gmail.com>
2023-10-09 15:37:09 +0900

    #onb-12 base tableView cell 정의

---
+++ onboard-iOS/BaseComponents/BaseTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  BaseTableViewCell.swift
+//  onboard-iOS
+//
+//  Created by Daye on 2023/10/09.
+//
+
+import UIKit
+
+class BaseTableViewCell<T>: UITableViewCell {
+
+  // MARK: - Initialize
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.selectionStyle = .none
+    self.setupConstraints()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  var model: T? {
+    didSet {
+      if let model = model {
+        bind(model)
+      }
+    }
+  }
+
+  func bind(_ model: T?) { /* override point */ }
+
+  func setupConstraints() { /* override point */ }
+}
---
+++ onboard-iOS/Presentation/SignIn/Views/TermsAgreementCell.swift
@@ -0,0 +1,12 @@
+//
+//  TermsAgreementCell.swift
+//  onboard-iOS
+//
+//  Created by Daye on 2023/10/09.
+//
+
+import UIKit
+
+final class TermsAgreementCell: BaseTableViewCell<String> {
+
+}
