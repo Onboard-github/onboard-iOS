31e50b545794fb4a871c9cc8938f74478e7ce074
윤다예 <daye.elin.yoon@gmail.com>
2023-10-10 22:38:51 +0900

    #onb-12 baseTableViewCell 정의

--- onboard-iOS/BaseComponents/BaseTableViewCell.swift
+++ onboard-iOS/BaseComponents/BaseTableViewCell.swift
@@ -9,27 +9,27 @@ import UIKit
 
 class BaseTableViewCell<T>: UITableViewCell {
 
-  // MARK: - Initialize
+    // MARK: - Initialize
 
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: style, reuseIdentifier: reuseIdentifier)
-    self.selectionStyle = .none
-    self.setupConstraints()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  var model: T? {
-    didSet {
-      if let model = model {
-        bind(model)
-      }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        self.setupConstraints()
     }
-  }
 
-  func bind(_ model: T?) { /* override point */ }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-  func setupConstraints() { /* override point */ }
+    var model: T? {
+        didSet {
+            if let model = model {
+                bind(model)
+            }
+        }
+    }
+
+    func bind(_ model: T?) { /* override point */ }
+
+    func setupConstraints() { /* override point */ }
 }
