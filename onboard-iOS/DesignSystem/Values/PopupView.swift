27b146e3a4f433aa5d22ab3f3645e76207a01f12
hyeri00 <nexusring@gmail.com>
2023-10-14 21:43:25 +0900

    Layout 추가

--- onboard-iOS/DesignSystem/Values/PopupView.swift
+++ onboard-iOS/DesignSystem/Values/PopupView.swift
@@ -118,4 +118,58 @@ class PopupView: UIView {
         view.alignment = .fill
         return view
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.backgroundView)
+        self.addSubview(self.contentView)
+        self.contentView.addSubview(self.headerStackView)
+        self.contentView.addSubview(self.textFieldStackView)
+        self.contentView.addSubview(self.registerButton)
+        
+        self.backgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        self.contentView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        self.headerStackView.snp.makeConstraints {
+            $0.top.equalTo(26)
+            $0.leading.equalTo(24)
+            $0.trailing.equalTo(-24)
+        }
+        
+        self.textField.snp.makeConstraints {
+            $0.height.equalTo(52)
+        }
+        
+        self.textFieldStackView.snp.makeConstraints {
+            $0.top.equalTo(headerStackView.snp.bottom).offset(15)
+            $0.leading.equalTo(24)
+            $0.trailing.equalTo(-24)
+        }
+        
+        self.registerButton.snp.makeConstraints {
+            $0.top.equalTo(textFieldStackView.snp.bottom).offset(20)
+            $0.bottom.equalTo(0)
+            $0.leading.equalTo(0)
+            $0.trailing.equalTo(0)
+            $0.height.equalTo(52)
+        }
+    }
 }
