26803fe5718a61a87d244891c546da33d0f77c4d
hyeri00 <nexusring@gmail.com>
2023-10-18 21:27:55 +0900

    TextField 수정

--- onboard-iOS/DesignSystem/Components/TextField.swift
+++ onboard-iOS/DesignSystem/Components/TextField.swift
@@ -12,32 +12,25 @@ class TextField: UITextField {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setColors()
         setupDefaults()
         setupBorder()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        layer.sublayers?.removeAll(where: { $0 is CAGradientLayer })
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = bounds
-        gradientLayer.colors = [Colors.Gray_2.cgColor, Colors.Gray_3.cgColor]
-        
-        layer.insertSublayer(gradientLayer, at: 0)
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+    private func setColors() {
+        self.tintColor = Colors.Orange_8
+        self.backgroundColor = Colors.Gray_2
+    }
+    
     private func setupDefaults() {
         self.font = Font.Typography.body2_R
         self.borderStyle = .roundedRect
+        self.layer.cornerRadius = 12
         self.keyboardType = .default
-        self.tintColor = Colors.Orange_8
-        self.backgroundColor = .clear
     }
     
     private func setupBorder() {
