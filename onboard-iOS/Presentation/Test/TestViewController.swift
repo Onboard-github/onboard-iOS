c87340263ef7ec21724a7e9f03740e07e893ba02
hyeri00 <nexusring@gmail.com>
2023-10-18 21:57:54 +0900

    버튼 액션 추가

--- onboard-iOS/Presentation/Test/TestViewController.swift
+++ onboard-iOS/Presentation/Test/TestViewController.swift
@@ -18,7 +18,7 @@ final class TestViewController: UIViewController, View {
     var disposeBag = DisposeBag()
 
     private let testView = TestView()
-    private let alertView = PopupView()
+    private let popupView = PopupView()
 
     // MARK: - Life Cycles
 
@@ -33,6 +33,13 @@ final class TestViewController: UIViewController, View {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configurePopup() {
+        view.addSubview(popupView)
+        popupView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Bind
@@ -57,7 +64,7 @@ final class TestViewController: UIViewController, View {
         }
         
         self.testView.didTapProfileButton = { [self] in
-            view.addSubview(alertView)
+            configurePopup()
             
             let popupState = PopupState(titleLabel: "프로필 설정",
                                         subTitleLabel: "모임에서 사용할 닉네임을 10자 이하로 입력해주세요.",
@@ -67,12 +74,27 @@ final class TestViewController: UIViewController, View {
                                         buttonLabel: "모임 등록하기",
                                         linkButtonState: LinkButtonState(isLink: true))
             
-            alertView.setState(popupState: popupState, onClickLink: { })
-            alertView.snp.makeConstraints {
-                $0.edges.equalToSuperview()
-            }
-            alertView.contentView.snp.makeConstraints {
+            popupView.setState(popupState: popupState, onClickLink: { })
+            popupView.contentView.snp.makeConstraints {
                 $0.height.equalTo(228)
+            }
+        }
+        
+        self.testView.didTapMemberButton = { [self] in
+            configurePopup()
+            
+            let popupState = PopupState(titleLabel: "임시 멤버 추가",
+                                        subTitleLabel: "임시 멤버란 이런이런이유로 이런이런 사람들을 이렇게 먼저 \n들어와있게 해주는거고 나중에 가입하면 변환도 가능",
+                                        textFieldPlaceholder: "닉네임을 입력해주세요.",
+                                        textFieldSubTitleLabel: "한글, 영문, 숫자를 조합하여 사용 가능합니다.",
+                                        countLabel: "0/10",
+                                        buttonLabel: "모임 등록하기",
+                                        linkButtonState: LinkButtonState(isLink: false),
+                                        textFieldLabelState: TextFieldLabelState(isHidden: true))
+            
+            popupView.setState(popupState: popupState, onClickLink: { })
+            popupView.contentView.snp.makeConstraints {
+                $0.height.equalTo(272)
             }
         }
     }
