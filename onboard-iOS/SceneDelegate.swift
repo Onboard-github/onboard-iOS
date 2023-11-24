b1567aa6a466360f75902813d8c17cff803a584f
higgs1022 <csh8130@kakao.com>
2023-11-24 17:58:06 +0900

    [ONB-8] 로그인 선택화면 디자인 적용

--- onboard-iOS/SceneDelegate.swift
+++ onboard-iOS/SceneDelegate.swift
@@ -40,7 +40,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         //let testViewController = GroupSearchViewController(reactor: GroupSearchReactor(useCase: testUseCase))
         let testViewController = TestViewController(reactor: testReactor)
 
-        self.window?.rootViewController = testViewController
+        self.window?.rootViewController = LoginSelectVC()
         
         self.window?.makeKeyAndVisible()
     }
