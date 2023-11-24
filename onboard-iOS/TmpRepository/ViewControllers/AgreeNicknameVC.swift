f8210b1bc9a36c1d090cc181da4886606140a962
higgs1022 <csh8130@kakao.com>
2023-11-25 00:26:20 +0900

    [ONB-90] 닉네임 설정화면 글자수 체크

--- onboard-iOS/TmpRepository/ViewControllers/AgreeNicknameVC.swift
+++ onboard-iOS/TmpRepository/ViewControllers/AgreeNicknameVC.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class AgreeNicknameVC: UIViewController {
+    @IBOutlet weak var nickNameField: UITextField!
+    @IBOutlet weak var maxLabel: UILabel!
     @IBOutlet weak var confirmButton: BorderedButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nickNameField.delegate = self
     }
     
     @IBAction func confirmButtonAction(_ sender: Any) {
+    }
+}
+
+extension AgreeNicknameVC: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 최대 글자 수를 설정합니다.
+        let maxLength = 10
+        
+        // 입력된 글자 수를 계산합니다.
+        let currentText = textField.text ?? ""
+        let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        
+        // 최대 글자 수를 초과하는지 확인하고 초과한다면 입력을 허용하지 않습니다.
+        return newText.count <= maxLength
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        maxLabel.text = "\(textField.text?.count ?? 0)/10"
+        //            print("현재 글자 수: \(textField.text?.count ?? 0)")
     }
 }
