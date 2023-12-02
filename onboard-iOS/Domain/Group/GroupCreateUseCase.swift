e309da3139e90c9344d77aca359190447edc2626
hyeri00 <nexusring@gmail.com>
2023-12-03 03:28:43 +0900

    GroupCreateUseCaseImpl class 구현 및 랜덤 이미지 가져오는 비지니스 로직 구현

--- onboard-iOS/Domain/Group/GroupCreateUseCase.swift
+++ onboard-iOS/Domain/Group/GroupCreateUseCase.swift
@@ -10,3 +10,20 @@ import Foundation
 protocol GroupCreateUseCase {
     func fetchRandomImage() async throws -> GroupCreateEntity
 }
+
+final class GroupCreateUseCaseImpl: GroupCreateUseCase {
+    
+    private let repository: GroupCreateRepository
+    
+    init(repository: GroupCreateRepository) {
+        self.repository = repository
+    }
+    
+    func fetchRandomImage() async throws -> GroupCreateEntity {
+        do {
+            return try await self.repository.requestRandomImage()
+        } catch {
+            throw error
+        }
+    }
+}
