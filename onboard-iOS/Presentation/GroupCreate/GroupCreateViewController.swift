a9b54bfb71f942012a45357cd8b9359cab485c2f
hyeri00 <nexusring@gmail.com>
2023-12-03 03:55:11 +0900

    viewDidLoad -> init 변경

--- onboard-iOS/Presentation/GroupCreate/GroupCreateViewController.swift
+++ onboard-iOS/Presentation/GroupCreate/GroupCreateViewController.swift
@@ -24,10 +24,16 @@ final class GroupCreateViewController: UIViewController {
         self.view = groupCreateView
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - Initialize
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
         
         self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func configure() {
