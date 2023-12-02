07f10e94c166a683ac7cf2aea04628dfa2f53187
hyeri00 <nexusring@gmail.com>
2023-12-03 03:18:21 +0900

    api 응답 디코딩하는 데 사용되는 DTO 추가

---
+++ onboard-iOS/Repository/DTO/GroupCreateDTO.swift
@@ -0,0 +1,13 @@
+//
+//  GroupCreateDTO.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 12/3/23.
+//
+
+import Foundation
+
+struct GroupCreateDTO: Decodable {
+    let uuid: String
+    let url: String
+}
