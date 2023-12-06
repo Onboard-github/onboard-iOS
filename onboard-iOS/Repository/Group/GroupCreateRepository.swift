e899dbc9d3839afb3bfbc8a72166508da5110168
hyeri00 <nexusring@gmail.com>
2023-12-06 22:42:27 +0900

    requestFileUpload 통해 api 요청 처리

--- onboard-iOS/Repository/Group/GroupCreateRepository.swift
+++ onboard-iOS/Repository/Group/GroupCreateRepository.swift
@@ -8,10 +8,33 @@
 import Foundation
 
 protocol GroupCreateRepository {
+    func requestFileUpload(file: File, purpose: Purpose) async throws -> GroupCreateEntity.Res
     func requestRandomImage() async throws -> GroupCreateEntity.Res
 }
 
 final class GroupCreateRepositoryImpl: GroupCreateRepository {
+    
+    func requestFileUpload(file: File, purpose: Purpose) async throws -> GroupCreateEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncFileUploadRequest(
+                    object: GroupCreateDTO.self,
+                    router: OBRouter.pickerImage(
+                        params: ["file": file,
+                                 "purpose": purpose]),
+                        file: file
+                )
+            
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+            
+            return data.toDomain()
+        } catch {
+            throw error
+        }
+    }
     
     func requestRandomImage() async throws -> GroupCreateEntity.Res {
         do {
