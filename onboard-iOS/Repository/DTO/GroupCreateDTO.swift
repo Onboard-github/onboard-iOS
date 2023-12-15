a1cc34a4e25ac5f5f17d7e341ebe4d6807e386d8
hyeri00 <nexusring@gmail.com>
2023-12-15 19:09:36 +0900

    그룹 생성 DTO 추가

--- onboard-iOS/Repository/DTO/GroupCreateDTO.swift
+++ onboard-iOS/Repository/DTO/GroupCreateDTO.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 struct GroupCreateDTO: Decodable {
+    let content: [Group]
+    
+    struct Group: Codable {
+        let name: String
+        let description: String
+        let organization: String
+        let profileImageUrl: String?
+        let profileImageUuid: String
+        let nickname: String
+    }
+    
     let uuid: String
     let url: String
 }
