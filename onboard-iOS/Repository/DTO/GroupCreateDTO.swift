b27cfeada8236fea2d9fe66451f6a6747863900c
hyeri00 <nexusring@gmail.com>
2023-12-20 22:55:47 +0900

    DTO 리팩토링 및 CodingKeys 추가

--- onboard-iOS/Repository/DTO/GroupCreateDTO.swift
+++ onboard-iOS/Repository/DTO/GroupCreateDTO.swift
@@ -19,13 +19,23 @@ enum GroupCreateRequest {
 }
 
 struct GroupCreateCompleteDTO: Decodable {
-    let id: Int
-    let name: String
-    let description: String
-    let owner: String
-    let organization: String
-    let profileImageUrl: String
-    let accessCode: String
+    let id: Int?
+    let name: String?
+    let description: String?
+    let owner: String?
+    let organization: String?
+    let profileImageUrl: String?
+    let accessCode: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case description = "description"
+        case owner = "owner"
+        case organization = "organization"
+        case profileImageUrl = "profileImageUrl"
+        case accessCode = "accessCode"
+    }
 }
 
 struct GroupCreateDTO: Decodable {
