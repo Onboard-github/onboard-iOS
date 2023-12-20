ca457644dbf970702182c9c7e3d6557bb2f3d447
hyeri00 <nexusring@gmail.com>
2023-12-20 22:10:59 +0900

    feat: 그룹 생성 데이터 관리

--- onboard-iOS/Utils/GroupCreateManager.swift
+++ onboard-iOS/Utils/GroupCreateManager.swift
@@ -10,16 +10,18 @@ import Foundation
 class GroupCreateManager {
     
     static let shared = GroupCreateManager()
-
+    
     private let userDefaults = UserDefaults.standard
     
     private let nameKey = "nameKey"
     private let descriptionKey = "descriptionKey"
     private let organizationKey = "organizationKey"
+    private let ownerKey = "ownerKey"
+    private let urlKey = "urlKey"
     private let uuidKey = "profileImageUUIDKey"
     
     // MARK: - Set
-
+    
     func saveName(_ name: String) {
         userDefaults.set(name, forKey: nameKey)
     }
@@ -31,13 +33,21 @@ class GroupCreateManager {
     func saveOrganization(_ organization: String) {
         userDefaults.set(organization, forKey: organizationKey)
     }
-
+    
+    func saveOwner(_ owner: String) {
+        userDefaults.set(owner, forKey: ownerKey)
+    }
+    
+    func saveUrl(_ url: String) {
+        userDefaults.set(url, forKey: urlKey)
+    }
+    
     func saveUUID(_ uuid: String) {
         userDefaults.set(uuid, forKey: uuidKey)
     }
     
     // MARK: - Get
-
+    
     func getName() -> String? {
         return userDefaults.string(forKey: nameKey)
     }
@@ -48,6 +58,14 @@ class GroupCreateManager {
     
     func getOrganization() -> String? {
         return userDefaults.string(forKey: organizationKey)
+    }
+    
+    func getOwner() -> String? {
+        return userDefaults.string(forKey: ownerKey)
+    }
+    
+    func getUrl() -> String? {
+        return userDefaults.string(forKey: urlKey)
     }
     
     func getSavedUUID() -> String? {
