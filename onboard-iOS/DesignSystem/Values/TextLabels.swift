16cf5fa262a0f2c5d7db5a4d0b9de1b044bc12e5
hyeri00 <nexusring@gmail.com>
2023-12-22 22:22:27 +0900

    그룹 생성 완료 text 레이블 정의

--- onboard-iOS/DesignSystem/Values/TextLabels.swift
+++ onboard-iOS/DesignSystem/Values/TextLabels.swift
@@ -9,4 +9,11 @@ import Foundation
 
 struct TextLabels {
     
+    // MARK: - Group Create
+    
+    static let group_complete_text = "그룹 등록이 완료되었습니다!"
+    static let group_owner_text = "관리자"
+    static let group_accessCode_text = "참여코드"
+    static let group_invite_text = "그룹에 가입하기 위해서는 참여코드를 입력해야 합니다.\n코드를 공유하고 멤버들을 초대해보세요!"
+    static let confirm_text = "확인"
 }
