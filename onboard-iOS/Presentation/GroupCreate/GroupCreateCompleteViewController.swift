15f582cbfbade746e006b004e392cf4ec2d157f8
hyeri00 <nexusring@gmail.com>
2024-01-06 23:55:37 +0900

    reactor 적용

--- onboard-iOS/Presentation/GroupCreate/GroupCreateCompleteViewController.swift
+++ onboard-iOS/Presentation/GroupCreate/GroupCreateCompleteViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-final class GroupCreateCompleteViewController: UIViewController {
+import ReactorKit
+
+final class GroupCreateCompleteViewController: UIViewController, View {
+    
+    typealias Reactor = GroupCreateReactor
+    
+    // MARK: - Properties
+    
+    var disposeBag = DisposeBag()
     
     private let groupCreateCompleteView = GroupCreateCompleteView()
+    
+    // MARK: - Life Cycles
     
     override func loadView() {
         super.loadView()
         
         self.view = groupCreateCompleteView
+    }
+    
+    // MARK: - Initialize
+    
+    init(reactor: GroupCreateReactor) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.reactor = reactor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: GroupCreateReactor) {
+        self.bindAction(reactor: reactor)
+        self.bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: GroupCreateReactor) {
+        
+    }
+    
+    func bindState(reactor: GroupCreateReactor) {
+        reactor.state
+            .compactMap { $0.createdGroup }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] data in
+            })
+            .disposed(by: disposeBag)
     }
 }
