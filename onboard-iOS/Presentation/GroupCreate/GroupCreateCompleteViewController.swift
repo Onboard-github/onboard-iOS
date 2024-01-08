a4469320ab06ddd07cb5362f4a2da6077d00a5a4
hyeri00 <nexusring@gmail.com>
2024-01-09 00:02:43 +0900

    bindAction 제거

--- onboard-iOS/Presentation/GroupCreate/GroupCreateCompleteViewController.swift
+++ onboard-iOS/Presentation/GroupCreate/GroupCreateCompleteViewController.swift
@@ -42,20 +42,16 @@ final class GroupCreateCompleteViewController: UIViewController, View {
     // MARK: - Bind
     
     func bind(reactor: GroupCreateReactor) {
-        self.bindAction(reactor: reactor)
         self.bindState(reactor: reactor)
     }
     
-    func bindAction(reactor: GroupCreateReactor) {
-        
-    }
-    
     func bindState(reactor: GroupCreateReactor) {
-        reactor.state
-            .compactMap { $0.createdGroup }
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] data in
-            })
-            .disposed(by: disposeBag)
-    }
+         reactor.state
+             .map { $0.createdGroup }
+             .compactMap { $0 }
+             .observe(on: MainScheduler.instance)
+             .subscribe(onNext: { [weak self] data in
+             })
+             .disposed(by: disposeBag)
+     }
 }
