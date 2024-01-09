288112e8ae0968ba573c2c5ed1b85e779349a222
hyeri00 <nexusring@gmail.com>
2024-01-09 18:19:28 +0900

    설정화면 TableViewCell Layout 추가

--- onboard-iOS/Presentation/GroupSetting/Views/Cells/GroupSettingTableViewCell.swift
+++ onboard-iOS/Presentation/GroupSetting/Views/Cells/GroupSettingTableViewCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 final class GroupSettingTableViewCell: UITableViewCell {
     
+    // MARK: - Metric
+    
+    private enum Metric {
+        static let LeftMargin: CGFloat = 30
+        static let separatorMargin: CGFloat = 14
+        static let separatorHeight: CGFloat = 1
+    }
+    
     // MARK: - UI
     
     let label: UILabel = {
@@ -23,4 +31,39 @@ final class GroupSettingTableViewCell: UITableViewCell {
         view.backgroundColor = Colors.Gray_5
         return view
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.selectionStyle = .none
+        
+        self.makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.label)
+        self.addSubview(self.separatorView)
+        
+        self.label.snp.makeConstraints {
+            $0.leading.equalTo(Metric.LeftMargin)
+            $0.centerY.equalToSuperview()
+        }
+        
+        self.separatorView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(Metric.separatorMargin)
+            $0.height.equalTo(Metric.separatorHeight)
+        }
+    }
 }
