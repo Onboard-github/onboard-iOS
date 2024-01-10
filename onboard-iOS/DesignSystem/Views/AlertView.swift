9043d92ef6be9dad17dc471200a449697964fe7f
hyeri00 <nexusring@gmail.com>
2024-01-10 20:53:57 +0900

    AlertView Layout 추가

--- onboard-iOS/DesignSystem/Views/AlertView.swift
+++ onboard-iOS/DesignSystem/Views/AlertView.swift
@@ -9,6 +9,16 @@ import UIKit
 
 final class AlertView: UIView {
     
+    // MARK: - Metric
+    
+    private enum Metric {
+        static let LRMargin: CGFloat = 24
+        static let contentViewHeight: CGFloat = 216
+        static let labelTopMargin: CGFloat = 30
+        static let buttonTopMargin: CGFloat = 20
+        static let buttonHeight: CGFloat = 52
+    }
+    
     // MARK: - UI
     
     private let backgroundView: UIView = {
@@ -49,4 +59,59 @@ final class AlertView: UIView {
         button.backgroundColor = Colors.Orange_10
         return button
     }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.backgroundView)
+        self.backgroundView.addSubview(self.contentView)
+        self.contentView.addSubview(self.contentLabel)
+        self.contentView.addSubview(self.leftButton)
+        self.contentView.addSubview(self.rightButton)
+        
+        self.backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        self.contentView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(Metric.LRMargin)
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(Metric.contentViewHeight)
+        }
+        
+        self.contentLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(Metric.labelTopMargin)
+            $0.leading.trailing.equalToSuperview().inset(Metric.LRMargin)
+        }
+        
+        self.leftButton.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(Metric.buttonTopMargin)
+            $0.bottom.leading.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.5)
+            $0.height.equalTo(Metric.buttonHeight)
+        }
+        
+        self.rightButton.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(Metric.buttonTopMargin)
+            $0.bottom.trailing.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.5)
+            $0.height.equalTo(Metric.buttonHeight)
+        }
+    }
 }
