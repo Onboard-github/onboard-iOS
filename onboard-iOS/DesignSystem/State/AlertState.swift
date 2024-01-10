ec7092f5fa728e0e0333ebb6d4d4beeca9f53b9b
hyeri00 <nexusring@gmail.com>
2024-01-10 20:55:39 +0900

    AlertState 정의

---
+++ onboard-iOS/DesignSystem/State/AlertState.swift
@@ -0,0 +1,14 @@
+//
+//  AlertState.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 1/10/24.
+//
+
+import Foundation
+
+struct AlertState {
+    let contentLabel: String
+    let leftButtonLabel: String
+    let rightButtonLabel: String
+}
