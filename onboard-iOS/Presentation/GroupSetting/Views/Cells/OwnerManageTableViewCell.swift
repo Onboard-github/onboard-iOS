e8c4d5cf1284e81fda59403bfa0c0eb1f97759c5
hyeri00 <nexusring@gmail.com>
2024-01-10 21:27:17 +0900

    관리자 변경 TableViewCell Layout 추가

--- onboard-iOS/Presentation/GroupSetting/Views/Cells/OwnerManageTableViewCell.swift
+++ onboard-iOS/Presentation/GroupSetting/Views/Cells/OwnerManageTableViewCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 final class OwnerManageTableViewCell: UITableViewCell {
     
+    // MARK: - Metric
+    
+    private enum Metric {
+        static let leftMargin: CGFloat = 24
+        static let labelLeftSpacing: CGFloat = 14
+        static let buttonRightMargin: CGFloat = 24
+    }
+    
     // MARK: - UI
     
     private let titleImage: UIImageView = {
@@ -30,4 +38,45 @@ final class OwnerManageTableViewCell: UITableViewCell {
         button.setImage(IconImage.selected.image, for: .normal)
         return button
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.selectionStyle = .none
+        
+        self.makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.titleImage)
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.selectButton)
+        
+        self.titleImage.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(Metric.leftMargin)
+            $0.centerY.equalToSuperview()
+        }
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(titleImage.snp.trailing).offset(Metric.labelLeftSpacing)
+            $0.centerY.equalToSuperview()
+        }
+        
+        self.selectButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(Metric.buttonRightMargin)
+            $0.centerY.equalToSuperview()
+        }
+    }
 }
