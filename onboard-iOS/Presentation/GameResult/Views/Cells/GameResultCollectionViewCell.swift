2a2feb16eabcdfa872630d24d50587aade276d26
hyeri00 <nexusring@gmail.com>
2024-01-13 23:42:19 +0900

    게임 결과 CollectionViewCell Layout 추가

--- onboard-iOS/Presentation/GameResult/Views/Cells/GameResultCollectionViewCell.swift
+++ onboard-iOS/Presentation/GameResult/Views/Cells/GameResultCollectionViewCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 final class GameResultCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - Metric
+    
+    private enum Metric {
+        static let imageWidth: CGFloat = 102
+        static let imageHeight: CGFloat = 132
+        static let labelTopSpacing: CGFloat = 5
+    }
+    
     // MARK: - UI
     
     private let gameImage: UIImageView = {
@@ -26,4 +34,38 @@ final class GameResultCollectionViewCell: UICollectionViewCell {
         label.textAlignment = .left
         return label
     }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = Colors.White
+        
+        self.makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.gameImage)
+        self.addSubview(self.gameLabel)
+        
+        self.gameImage.snp.makeConstraints {
+            $0.width.equalTo(Metric.imageWidth)
+            $0.height.equalTo(Metric.imageHeight)
+        }
+        
+        self.gameLabel.snp.makeConstraints {
+            $0.top.equalTo(gameImage.snp.bottom).offset(Metric.labelTopSpacing)
+        }
+    }
 }
