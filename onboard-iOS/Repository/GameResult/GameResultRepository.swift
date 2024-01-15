d1c5ce9c74d2a766595a89db5784a03bf2a80b1e
hyeri00 <nexusring@gmail.com>
2024-01-15 22:51:21 +0900

    게임 결과를 비동기식으로 가져오기 위한 GameResultRepositoryImpl 추가

--- onboard-iOS/Repository/GameResult/GameResultRepository.swift
+++ onboard-iOS/Repository/GameResult/GameResultRepository.swift
@@ -10,3 +10,27 @@ import Foundation
 protocol GameResultRepository {
     func requestResult(groupId: Int, sort: String) async throws -> GameResultEntity.Res
 }
+
+final class GameResultRepositoryImpl: GameResultRepository {
+    
+    func requestResult(groupId: Int, sort: String) async throws -> GameResultEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: GameResultDTO.self,
+                    router: OBRouter.gameResult(
+                        params: ["groupId": groupId,
+                                 "sort": sort]
+                    )
+                )
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+            
+            return data.toDomain()
+        } catch {
+            throw error
+        }
+    }
+}
