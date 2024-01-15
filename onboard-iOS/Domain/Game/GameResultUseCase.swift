2ad834c79a80e6b15fcee3effe7616afedce8e25
hyeri00 <nexusring@gmail.com>
2024-01-15 23:09:40 +0900

    결과 처리하는 GameResultUseCaseImpl 생성

--- onboard-iOS/Domain/Game/GameResultUseCase.swift
+++ onboard-iOS/Domain/Game/GameResultUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol GameResultUseCase {
     func fetchResult(groupId: Int, sort: String) async throws -> GameResultEntity.Res
 }
+
+final class GameResultUseCaseImpl: GameResultUseCase {
+    
+    private let repository: GameResultRepository
+    
+    init(repository: GameResultRepository) {
+        self.repository = repository
+    }
+    
+    func fetchResult(groupId: Int, sort: String) async throws -> GameResultEntity.Res {
+        try await self.repository.requestResult(groupId: groupId, sort: sort)
+    }
+}
