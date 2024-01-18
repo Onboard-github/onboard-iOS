d6969cab9cb1ae15aad636d126200bdfe3ce52a3
hyeri00 <nexusring@gmail.com>
2024-01-18 19:37:56 +0900

    PlayerUseCasempl 정의

--- onboard-iOS/Domain/Game/PlayerUseCase.swift
+++ onboard-iOS/Domain/Game/PlayerUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol PlayerUseCase {
     func fetchPlayerList(groupId: Int, size: String) async throws -> PlayerEntity.Res
 }
+
+final class PlayerUseCasempl: PlayerUseCase {
+    
+    private let repository: PlayerRepository
+    
+    init(repository: PlayerRepository) {
+        self.repository = repository
+    }
+    
+    func fetchPlayerList(groupId: Int, size: String) async throws -> PlayerEntity.Res {
+        try await self.repository.requestPlayerList(groupId: groupId, size: size)
+    }
+}
