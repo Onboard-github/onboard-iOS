c9bc7d985a1873b104e6c88d4ece6be902330c93
hyeri00 <nexusring@gmail.com>
2024-01-18 19:34:57 +0900

    PlayerRepositoryImpl 정의

--- onboard-iOS/Repository/GameResult/PlayerRepository.swift
+++ onboard-iOS/Repository/GameResult/PlayerRepository.swift
@@ -10,3 +10,25 @@ import Foundation
 protocol PlayerRepository {
     func requestPlayerList(groupId: Int, size: String) async throws -> PlayerEntity.Res
 }
+
+final class PlayerRepositoryImpl: PlayerRepository {
+    func requestPlayerList(groupId: Int, size: String) async throws -> PlayerEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: PlayerDTO.self,
+                    router: OBRouter.gamePlayer(
+                        params: ["groupId": groupId,
+                                 "size": size]
+                    )
+                )
+            
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+        } catch {
+            throw error
+        }
+    }
+}
