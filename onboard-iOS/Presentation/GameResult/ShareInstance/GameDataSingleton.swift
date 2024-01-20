47b744ed45ba67fa85cec51e5057f05ed4d13729
hyeri00 <nexusring@gmail.com>
2024-01-20 21:19:40 +0900

    playerData 도입

--- onboard-iOS/Presentation/GameResult/ShareInstance/GameDataSingleton.swift
+++ onboard-iOS/Presentation/GameResult/ShareInstance/GameDataSingleton.swift
@@ -12,10 +12,20 @@ import UIKit
 class GameDataSingleton {
     
     static let shared = GameDataSingleton()
-    
+
     var gameData: GameResultEntity.Res.GameList?
-    
+    var playerData: [PlayerList] = []
+
     private init() {}
+
+    func addSelectedPlayer(_ player: PlayerList) {
+        self.playerData.append(player)
+    }
+
+    func removePlayer(at index: Int) {
+        guard index < self.playerData.count else { return }
+        self.playerData.remove(at: index)
+    }
 }
 
 struct PlayerList {
