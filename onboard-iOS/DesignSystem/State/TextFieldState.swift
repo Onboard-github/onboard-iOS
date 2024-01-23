3d44243daaf015c6e81f21472da8985309a4337f
hyeri00 <nexusring@gmail.com>
2024-01-23 18:18:10 +0900

    TextFieldState 정의

---
+++ onboard-iOS/DesignSystem/State/TextFieldState.swift
@@ -0,0 +1,14 @@
+//
+//  TextFieldState.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 1/23/24.
+//
+
+import Foundation
+
+enum TextFieldState {
+    case normal
+    case over
+    case overLap
+}
