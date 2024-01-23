4f105012ab15c449ab2e896a57d52c1ae7a1b371
hyeri00 <nexusring@gmail.com>
2024-01-23 18:52:57 +0900

    BottomSheet KeyboardHandlingViewController 적용

--- onboard-iOS/DesignSystem/Views/BottomSheetViewController.swift
+++ onboard-iOS/DesignSystem/Views/BottomSheetViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class BottomSheetViewController: UIViewController {
+class BottomSheetViewController: KeyboardHandlingViewController {
     
     // MARK: - Properties
     
