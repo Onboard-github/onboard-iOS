0dff3aa6604b259c1e2144d2fe2221fb03899c09
hyeri00 <nexusring@gmail.com>
2024-01-24 23:46:41 +0900

    [ONB-143] GuestNickNmaeEntity 생성

---
+++ onboard-iOS/Domain/Entity/GuestNickNameEntity.swift
@@ -0,0 +1,15 @@
+//
+//  GuestNickNameEntity.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 1/24/24.
+//
+
+import Foundation
+
+struct GuestNickNameEntity {
+    struct Res {
+        let isAvailable: Bool
+        let reason: String
+    }
+}
