5e6d081142a8ad8bc390d15745877867b2a0f015
hyeri00 <nexusring@gmail.com>
2024-01-24 23:51:25 +0900

    [ONB-143] GuestRepositoryImpl 생성 (GuestRepository protocol 채택)

--- onboard-iOS/Repository/Game/GuestRepository.swift
+++ onboard-iOS/Repository/Game/GuestRepository.swift
@@ -10,3 +10,30 @@ import Foundation
 protocol GuestRepository {
     func requestValidateNickName(groupId: Int, nickname: String) async throws -> GuestNickNameEntity.Res
 }
+
+final class GuestRepositoryImpl: GuestRepository {
+    
+    func requestValidateNickName(groupId: Int, nickname: String) async throws -> GuestNickNameEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: GuestNicknameDTO.self,
+                    router: OBRouter.validateNicknameGuest(
+                        params: [
+                            "groupId": groupId,
+                            "nickname": nickname
+                        ]
+                    )
+                )
+            
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+            
+            return data.toDomain()
+        } catch {
+            throw error
+        }
+    }
+}
