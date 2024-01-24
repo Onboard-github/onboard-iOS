76c6da2e870ecd716036698e25792b3ba36306c6
hyeri00 <nexusring@gmail.com>
2024-01-24 23:56:08 +0900

    [ONB-143] GuestUseCaseImpl 생성 (GuestUseCase protocol 채택)

--- onboard-iOS/Domain/Game/GuestUseCase.swift
+++ onboard-iOS/Domain/Game/GuestUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol GuestUseCase {
     func fetchValidateNickName(groupId: Int, nickname: String) async throws -> GuestNickNameEntity.Res
 }
+
+final class GuestUseCaseImpl: GuestUseCase {
+    
+    private let repository: GuestRepository
+    
+    init(repository: GuestRepository) {
+        self.repository = repository
+    }
+    
+    func fetchValidateNickName(groupId: Int, nickname: String) async throws -> GuestNickNameEntity.Res {
+        try await self.repository.requestValidateNickName(groupId: groupId, nickname: nickname)
+    }
+}
