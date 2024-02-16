41f84396cda6d7776c05b866fdcf1521ae20cda6
hyeri00 <nexusring@gmail.com>
2024-02-16 23:12:51 +0900

    [ONB-14] GroupInfoDetailViewController Reactor 적용

--- onboard-iOS/Presentation/GroupInfo/GroupInfoDetailViewController.swift
+++ onboard-iOS/Presentation/GroupInfo/GroupInfoDetailViewController.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
-final class GroupInfoDetailViewController: UIViewController {
+import ReactorKit
+
+final class GroupInfoDetailViewController: UIViewController, View {
+    
+    typealias Reactor = GroupReactor
+    
+    // MARK: - Properties
+    
+    var disposeBag = DisposeBag()
     
     private let groupInfoDetailView = GroupInfoDetailView()
     
@@ -17,5 +25,55 @@ final class GroupInfoDetailViewController: UIViewController {
         super.loadView()
         
         self.view = groupInfoDetailView
+    }
+    
+    // MARK: - Initialize
+    
+    init(reactor: GroupReactor) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.reactor = reactor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Bind
+    
+    func bind(reactor: GroupReactor) {
+        self.bindAction(reactor: reactor)
+        self.bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: GroupReactor) {
+        let groupId = GameDataSingleton.shared.getGroupId() ?? 0
+        self.reactor?.action.onNext(.fetchResult(groupId: groupId))
+    }
+        
+    func bindState(reactor: GroupReactor) {
+        reactor.state
+            .compactMap { $0.groupInfoData }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] data in
+                ImageLoader.loadImage(from: data.profileImageUrl) { [weak self] image in
+                    DispatchQueue.main.async {
+                        guard let image = image else { return }
+                        self?.groupInfoDetailView.configureGroupInfo(
+                            name: data.name,
+                            titleImage: image,
+                            description: data.description,
+                            organization: data.organization,
+                            memberCount: data.memberCount,
+                            owner: data.owner.nickname,
+                            accessCode: data.accessCode,
+                            nickname: "닝",
+                            playCount: 5
+                        )
+                    }
+                    
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
