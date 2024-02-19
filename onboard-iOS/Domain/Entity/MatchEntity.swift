960fca832440ca117fafebceb869c850c9933c10
hyeri00 <nexusring@gmail.com>
2024-02-19 23:50:43 +0900

    [ONB-140] MatchEntity 작성

---
+++ onboard-iOS/Domain/Entity/MatchEntity.swift
@@ -0,0 +1,23 @@
+//
+//  MatchEntity.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 2/19/24.
+//
+
+import Foundation
+
+enum MatchEntity {
+    struct Req {
+        let gameId: Int
+        let groupId: Int
+        let matchedDate: String
+        
+        let matchMembers: [MatchRequest.Body.Match]
+    }
+    
+    struct Res {
+        
+    }
+}
+\
