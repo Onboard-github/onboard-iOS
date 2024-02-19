bec65d6b362e0e87bd431e70d758a49098470bdc
hyeri00 <nexusring@gmail.com>
2024-02-19 23:53:00 +0900

    [ONB-140] MatchRepositoryImpl 생성

--- onboard-iOS/Repository/Game/MatchRepository.swift
+++ onboard-iOS/Repository/Game/MatchRepository.swift
@@ -10,3 +10,38 @@ import Foundation
 protocol MatchRepository {
     func requestMatch(req: MatchEntity.Req) async throws -> MatchEntity.Res
 }
+
+final class MatchRepositoryImpl: MatchRepository {
+    
+    func requestMatch(req: MatchEntity.Req) async throws -> MatchEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: MatchDTO.self,
+                    router: OBRouter.match(
+                        body: MatchRequest.Body(
+                            gameId: req.gameId,
+                            groupId: req.groupId,
+                            matchedDate: req.matchedDate,
+                            matchMembers: req.matchMembers
+                        ).encode()
+                    )
+                )
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+            
+            return data.toDomain()
+        } catch {
+            throw error
+        }
+    }
+}
+
+
+extension MatchDTO {
+    func toDomain() -> MatchEntity.Res {
+        return MatchEntity.Res()
+    }
+}
