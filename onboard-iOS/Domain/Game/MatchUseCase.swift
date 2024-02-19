869599eebe8c6fdd5142a680ed4c411d1bfc6139
hyeri00 <nexusring@gmail.com>
2024-02-19 23:54:07 +0900

    [ONB-140] MatchUseCaseImpl 생성

--- onboard-iOS/Domain/Game/MatchUseCase.swift
+++ onboard-iOS/Domain/Game/MatchUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol MatchUseCase {
     func fetchMatch(req: MatchEntity.Req) async throws -> MatchEntity.Res
 }
+
+final class MatchUseCaseImpl: MatchUseCase {
+    
+    private let repository: MatchRepositoryImpl
+    
+    init(repository: MatchRepositoryImpl) {
+        self.repository = repository
+    }
+    
+    func fetchMatch(req: MatchEntity.Req) async throws -> MatchEntity.Res {
+        try await self.repository.requestMatch(req: req)
+    }
+}
