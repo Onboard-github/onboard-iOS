eb1eeec357797f07225d78977c524c3ba1094625
hyeri00 <nexusring@gmail.com>
2024-02-19 23:49:45 +0900

    [ONB-140] MatchRequest 작성

--- onboard-iOS/Repository/DTO/MatchDTO.swift
+++ onboard-iOS/Repository/DTO/MatchDTO.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+enum MatchRequest {
+    struct Body: Encodable {
+        let gameId: Int
+        let groupId: Int
+        let matchedDate: String
+        
+        let matchMembers: [Match]
+        
+        struct Match: Encodable {
+            let memberId: Int
+            let score: Int
+            let ranking: Int
+        }
+    }
+}
+
 struct MatchDTO: Decodable {
     
 }
