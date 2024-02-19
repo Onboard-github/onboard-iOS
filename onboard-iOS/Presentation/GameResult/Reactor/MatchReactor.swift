6ddfe98bf53ca2109564d2d9925ec46dde589775
hyeri00 <nexusring@gmail.com>
2024-02-19 23:55:17 +0900

    [ONB-140] Reactor 작성

---
+++ onboard-iOS/Presentation/GameResult/Reactor/MatchReactor.swift
@@ -0,0 +1,74 @@
+//
+//  MatchReactor.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 2/19/24.
+//
+
+import Foundation
+
+import ReactorKit
+
+final class MatchReactor: Reactor {
+    
+    var initialState: State = .init(recordMatchs: nil)
+    
+    enum Action {
+        case recordMatch(req: MatchEntity.Req)
+    }
+    
+    enum Mutation {
+        case setMatch(result: MatchEntity.Res)
+    }
+    
+    struct State {
+        var recordMatchs: MatchEntity.Res?
+    }
+    
+    private let useCase: MatchUseCase
+    
+    init(
+        useCase: MatchUseCase
+    ) {
+        self.useCase = useCase
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .recordMatch(req):
+            return self.recordMatchResult(req: req)
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+        var state = state
+        
+        switch mutation {
+        case .setMatch(let result):
+            state.recordMatchs = result
+        }
+        
+        return state
+    }
+}
+
+extension MatchReactor {
+    
+    private func recordMatchResult(req: MatchEntity.Req) -> Observable<Mutation> {
+        return Observable.create { [weak self] observer in
+            guard let self = self else { return Disposables.create() }
+            
+            Task {
+                do {
+                    let result = try await self.useCase.fetchMatch(req: req)
+                    
+                } catch {
+                    observer.onError(error)
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
