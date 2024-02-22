5a294a54ef0c5c6f7ade75b1241ed92a21910935
hyeri00 <nexusring@gmail.com>
2024-02-22 18:19:36 +0900

    [ONB-140] 검색 emptyView Layout 추가

--- onboard-iOS/Presentation/GameResult/Views/SearchEmptyStateView.swift
+++ onboard-iOS/Presentation/GameResult/Views/SearchEmptyStateView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class SearchEmptyStateView: UIView {
     
+    var didTapButton: (() -> Void)?
+    
     // MARK: - UI
     
     private let titleLabel: UILabel = {
@@ -32,4 +34,56 @@ final class SearchEmptyStateView: UIView {
         button.setImage(UIImage(named: "addGuest"), for: .normal)
         return button
     }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = Colors.White
+        
+        self.addConfigure()
+        self.makeConstraints()
+    }
+    
+    private func addConfigure() {
+        self.addButton.addAction(UIAction(handler: { [weak self] _ in
+            self?.didTapButton?()
+        }), for: .touchUpInside)
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.subTitleLabel)
+        self.addSubview(self.addButton)
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(150)
+        }
+        
+        self.subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+        }
+        
+        self.addButton.snp.makeConstraints {
+            $0.top.equalTo(self.subTitleLabel.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    func setTitle(title: String) {
+        self.titleLabel.text = title
+    }
 }
