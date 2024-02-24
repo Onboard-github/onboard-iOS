332fe979b7109fba5b721d6114a1cea5227b0c5e
hyeri00 <nexusring@gmail.com>
2024-02-24 13:05:09 +0900

    [ONB-142] refactor: 선택한 플레이어 리셋

--- onboard-iOS/Presentation/GameResult/GameResultViewController.swift
+++ onboard-iOS/Presentation/GameResult/GameResultViewController.swift
@@ -194,7 +194,7 @@ extension GameResultViewController: UICollectionViewDelegate, UICollectionViewDa
         }
         
         GameDataSingleton.shared.gameData = gameData
-        GameDataSingleton.shared.selectedPlayerData.removeAll()
+        GameDataSingleton.shared.gamePlayerData.removeAll()
         
         let useCase = PlayerUseCasempl(repository: PlayerRepositoryImpl())
         let reactor = PlayerReactor(useCase: useCase)
