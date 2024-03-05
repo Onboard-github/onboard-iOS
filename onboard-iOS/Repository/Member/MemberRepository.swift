7143dbe9468cc042369b6352c1e9e865dab8359f
hyeri00 <nexusring@gmail.com>
2024-03-05 14:36:45 +0900

    [ONB-138] feat: 그룹장 임명 기능 구현

--- onboard-iOS/Repository/Member/MemberRepository.swift
+++ onboard-iOS/Repository/Member/MemberRepository.swift
@@ -10,3 +10,28 @@ import Foundation
 protocol MemberRepository {
     func requestAssignOwner(groupId: Int, memberId: Int) async throws -> MemberEntity.Res
 }
+
+final class MemberRepositoryImpl: MemberRepository {
+    
+    func requestAssignOwner(groupId: Int, memberId: Int) async throws -> MemberEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: MemberDTO.self,
+                    router: OBRouter.assignOwner(
+                        groupId: groupId,
+                        memberId: memberId
+                    )
+                )
+            
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+            
+            return data.toDomain()
+        } catch {
+            throw error
+        }
+    }
+}
