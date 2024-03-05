a962cd262c7bbed84483b5bd2c835b430d62d7b2
hyeri00 <nexusring@gmail.com>
2024-03-05 14:39:34 +0900

    [ONB-138] feat: api 호출 후 fetch

--- onboard-iOS/Domain/Member/MemberUseCase.swift
+++ onboard-iOS/Domain/Member/MemberUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol MemberUseCase {
     func fetchAssignOwner(groupId: Int, memberId: Int) async throws -> MemberEntity.Res
 }
+
+final class MemberUseCaseImpl: MemberUseCase {
+    
+    private let repository: MemberRepository
+    
+    init(repository: MemberRepository) {
+        self.repository = repository
+    }
+    
+    func fetchAssignOwner(groupId: Int, memberId: Int) async throws -> MemberEntity.Res {
+        try await self.repository.requestAssignOwner(groupId: groupId, memberId: memberId)
+    }
+}
