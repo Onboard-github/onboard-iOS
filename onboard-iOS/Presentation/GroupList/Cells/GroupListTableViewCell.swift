3faf792bf625671157cfbeb612c9469f65f9fcf0
hyeri00 <nexusring@gmail.com>
2024-03-15 23:03:55 +0900

    [ONB-256] feat: backgroundColor 지정

--- onboard-iOS/Presentation/GroupList/Cells/GroupListTableViewCell.swift
+++ onboard-iOS/Presentation/GroupList/Cells/GroupListTableViewCell.swift
@@ -17,4 +17,22 @@ final class GroupListTableViewCell: UITableViewCell {
         label.font = Font.Typography.body2_R
         return label
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = Colors.White
+    }
 }
