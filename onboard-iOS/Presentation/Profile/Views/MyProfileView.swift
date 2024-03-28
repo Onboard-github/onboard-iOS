2647de504b40119176596802e8b081a866ef2e5a
hyeri00 <nexusring@gmail.com>
2024-03-28 23:46:24 +0900

    [ONB-258] feat: 모임 이름 UI 구성

---
+++ onboard-iOS/Presentation/Profile/Views/MyProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  MyProfileView.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 3/28/24.
+//
+
+import UIKit
+
+final class MyProfileView: UIView {
+    
+    // MARK: - UI
+    
+    private let groupLabel: UILabel = {
+        let label = UILabel()
+        label.text = TextLabels.profile_groupLabel
+        label.textColor = Colors.Gray_14
+        label.font = Font.Typography.body3_M
+        return label
+    }()
+    
+    private let groupNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.Gray_15
+        label.font = Font.Typography.title3
+        return label
+    }()
+}
