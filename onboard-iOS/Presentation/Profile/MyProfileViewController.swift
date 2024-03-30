c5f229936065f5e9160d38807b03fb589488756b
hyeri00 <nexusring@gmail.com>
2024-03-30 23:49:57 +0900

    [ONB-258] feat: navigationBar 추가

--- onboard-iOS/Presentation/Profile/MyProfileViewController.swift
+++ onboard-iOS/Presentation/Profile/MyProfileViewController.swift
@@ -20,4 +20,47 @@ final class MyProfileViewController: UIViewController {
         
         self.view = myProfileView
     }
+    
+    // MARK: - Initialize
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        
+        self.setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        let image = IconImage.back.image?.withTintColor(Colors.Black, renderingMode: .alwaysOriginal)
+        
+        if let navigationBar = navigationController?.navigationBar {
+            let titleTextAttributes: [NSAttributedString.Key: Any] = [
+                NSAttributedString.Key.font: Font.Typography.title2 as Any,
+                NSAttributedString.Key.foregroundColor: Colors.Gray_14
+            ]
+            navigationBar.titleTextAttributes = titleTextAttributes
+        }
+        
+        navigationController?.navigationBar.barTintColor = Colors.Gray_2
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: image, style: .done,
+            target: self, action: #selector(showPrevious))
+        navigationItem.title = TextLabels.profile_title
+    }
+    
+    @objc
+    private func showPrevious() {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
