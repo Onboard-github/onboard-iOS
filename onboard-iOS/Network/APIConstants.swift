ddf96142d9ed477012f2c93f8b416f90a2aee3f4
hyeri00 <nexusring@gmail.com>
2024-04-02 01:02:51 +0900

    refactor: Gif url 수정

--- onboard-iOS/Network/APIConstants.swift
+++ onboard-iOS/Network/APIConstants.swift
@@ -13,7 +13,7 @@ enum API {
 }
 
 enum GifURL {
-    static let url = "https://s3-alpha-sig.figma.com/img/158b/4197/dd2bc4fe7471b517be67f13ba9f178a2?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bS~gZt1umch5MDpVqUtFABZKB5Jf7r8UlrGcTFbbTvsiF-1eQAWmDNV0Hde9c-jGsLwMHWU2GST8jJJ4n~6ldOeUTekAno5CCNXEQB9fGxjOJs15DwCSRFv8lGbHJkSFE7Yzs9cuxVSWuYOdM~nPXi7VkYE8vwXRbNMaih7Dndb-eRMWghNCJklW3AW-fpgqP1MvF-KE0Aj-kldv4iY0QhvDL3JNZdwQoFEr60Gq9KJq0ccf66tMqXidzrtE~SIkideVTnXgSfiC3Mac9Gc2olDdVKbnwnllfG~W5z3zv31Jb-y~2dW0Bu-4a0yAKkprV-z8LI5I1aUmGgqFVYE3KA__"
+    static let url = "https://s3-alpha-sig.figma.com/img/fcdf/0f6f/658ee1272b9aff5a07773dd72a4d4354?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kpaFuMrbWxgO9GNK51hf9mdXafjIKTGDEuY7jFaT4~Js6-Xw75X4uZPknV9Zr4sz~FBo-I2M0OJFuq5IxsCkoKkdOY8c3CNqb6TQHU0G36n5Eqnvc8DFTWXTIru0-USU6nM4XwMABByjTeUdAW~e8t9epGkhC0LmmfB4u8QzrjeqZSOoYMLPP8Wgh2l-sYh4Abwbb~~p5FETqoI0TPxI0NXx1zIg7xhT6dBqd0eLgNmAqsV3QCQg49YcDM5jafkqvPRmkFTYk8aod8aXI119mzAS~3r~DazbGDarvh7jZaHKVjwaTENP5ef~7CRW91Sbf4uK4VMPkTfpqm9B24VFug__"
 }
 
 class APISession {
