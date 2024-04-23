526943276809946c1869bc0711659186f2b4c8d1
hyeri00 <nexusring@gmail.com>
2024-04-23 20:36:53 +0900

    [ONB-261] feat: UserUseCaseImpl 클래스 구현 - 사용자 정보 가져오기 기능

--- onboard-iOS/Domain/User/UserUseCase.swift
+++ onboard-iOS/Domain/User/UserUseCase.swift
@@ -10,3 +10,16 @@ import Foundation
 protocol UserUseCase {
     func fetchMeInfo(req: UpdateMyNicknameEntity.Req) async throws -> UpdateMyNicknameEntity.Res
 }
+
+final class UserUseCaseImpl: UserUseCase {
+    
+    private let repository: UserRepository
+    
+    init(repository: UserRepository) {
+        self.repository = repository
+    }
+    
+    func fetchMeInfo(req: UpdateMyNicknameEntity.Req) async throws -> UpdateMyNicknameEntity.Res {
+        try await self.repository.requestMeInfo(req: req)
+    }
+}
