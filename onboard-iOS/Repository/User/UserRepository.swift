ba9e2cd6bd8622f508032d9e2323b011e9e1ec33
hyeri00 <nexusring@gmail.com>
2024-04-23 20:31:47 +0900

    [ONB-261] feat: 비동기로 사용자 정보 업데이트 구현

--- onboard-iOS/Repository/User/UserRepository.swift
+++ onboard-iOS/Repository/User/UserRepository.swift
@@ -10,3 +10,29 @@ import Foundation
 protocol UserRepository {
     func requestMeInfo(req: UpdateMyNicknameEntity.Req) async throws -> UpdateMyNicknameEntity.Res
 }
+
+final class UserRepositoryImpl: UserRepository {
+    
+    func requestMeInfo(req: UpdateMyNicknameEntity.Req) async throws -> UpdateMyNicknameEntity.Res {
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: UserDTO.self,
+                    router: .setUser(
+                        body: UpdateMyNicknameRequest.Body(
+                            nickname: req.nickname
+                        ).encode()
+                    )
+                )
+            
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+            
+            return data.toDomain()
+        } catch {
+            throw error
+        }
+    }
+}
