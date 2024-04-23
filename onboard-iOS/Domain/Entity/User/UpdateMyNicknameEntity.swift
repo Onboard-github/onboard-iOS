92a8653b59b096a9117b74ae8cd959d6e76ea36d
hyeri00 <nexusring@gmail.com>
2024-04-23 20:27:37 +0900

    [ONB-261] feat: Entity 정의

---
+++ onboard-iOS/Domain/Entity/User/UpdateMyNicknameEntity.swift
@@ -0,0 +1,19 @@
+//
+//  UpdateMyNicknameEntity.swift
+//  onboard-iOS
+//
+//  Created by 혜리 on 4/23/24.
+//
+
+import Foundation
+
+enum UpdateMyNicknameEntity {
+    
+    struct Res {
+        
+    }
+    
+    struct Req {
+        let nickname: String
+    }
+}
